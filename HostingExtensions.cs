b2137f1a5bb4d2db27d7fd8b641772fd71f3d63d
lombeo <102901885+lombeo@users.noreply.github.com>
2024-12-08 22:12:57 +0700

    update authorize for swagger

--- HostingExtensions.cs
+++ HostingExtensions.cs
@@ -11,6 +11,7 @@ using System.ComponentModel.Design;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using Sep490_Backend.Services.AuthenService;
+using Microsoft.OpenApi.Models;
 
 namespace Sep490_Backend
 {
@@ -47,7 +48,6 @@ namespace Sep490_Backend
             builder.Services.AddHostedService<DefaultBackgroundService>();
 
             builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
             builder.Services.AddHealthChecks();
             builder.Services.AddCors(options =>
             {
@@ -58,6 +58,40 @@ namespace Sep490_Backend
                             .AllowAnyOrigin()
                             .AllowAnyMethod()
                             .AllowAnyHeader();
+                    });
+            });
+
+            builder.Services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "SEP490 API", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = @"JWT Authorization header using the Bearer scheme.
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      Example: 'Bearer 12345abcdef'",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+                  {
+                    {
+                      new OpenApiSecurityScheme
+                      {
+                        Reference = new OpenApiReference
+                          {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                          },
+                          Scheme = "oauth2",
+                          Name = "Bearer",
+                          In = ParameterLocation.Header,
+                        },
+                        new List<string>()
+                      }
                     });
             });
 
