4f23167f8284023b4d40f58624110658cd2d2eff
lombeo <102901885+lombeo@users.noreply.github.com>
2024-12-21 03:58:50 +0700

    feature: verifyOTP unit test

--- UnitTest-SEP490/AuthenUnitTest/SignUpUnitTest.cs
+++ UnitTest-SEP490/AuthenUnitTest/SignUpUnitTest.cs
@@ -8,12 +8,12 @@ using Message = Sep490_Backend.Infra.Constants.Message;
 
 namespace UnitTest_SEP490.AuthenUnitTest
 {
-    public class AuthenUnitTest
+    public class SignUpUnitTest
     {
         private readonly Mock<IAuthenService> _mockAuthenService;
         private readonly AuthenController _controller;
 
-        public AuthenUnitTest()
+        public SignUpUnitTest()
         {
             _mockAuthenService = new Mock<IAuthenService>();
             _controller = new AuthenController(_mockAuthenService.Object);
@@ -261,60 +261,6 @@ namespace UnitTest_SEP490.AuthenUnitTest
             Assert.Equal(Message.CommonMessage.ERROR_HAPPENED, result.Message);
 
             _mockAuthenService.Verify(service => service.SignUp(signUpDto), Times.Once);
-        }
-
-        [Fact]
-        public async Task VerifyOTP_WithValidOTP_ShouldReturnSuccessResponse()
-        {
-            // Arrange
-            var verifyOtpDto = new VerifyOtpDTO
-            {
-                OtpCode = "123456",
-                UserId = 1,
-                Reason = Sep490_Backend.Infra.Enums.ReasonOTP.SignUp
-            };
-
-            _mockAuthenService
-                .Setup(service => service.VerifyOTP(It.IsAny<VerifyOtpDTO>()))
-                .ReturnsAsync(true);
-
-            // Act
-            var result = await _controller.VerifyOTP(verifyOtpDto);
-
-            // Assert
-            Assert.True(result.Success);
-            Assert.True(result.Data);
-            Assert.Equal(Message.AuthenMessage.VERIFY_OTP_SUCCESS, result.Message);
-
-            _mockAuthenService.Verify(service => service.VerifyOTP(verifyOtpDto), Times.Once);
-        }
-
-        [Fact]
-        public async Task VerifyOTP_WithInvalidOTP_ShouldReturnErrorResponse()
-        {
-            // Arrange
-            var verifyOtpDto = new VerifyOtpDTO
-            {
-                OtpCode = "123456",
-                UserId = 1,
-                Reason = Sep490_Backend.Infra.Enums.ReasonOTP.SignUp
-            };
-
-            var expectedMessage = Message.AuthenMessage.INVALID_OTP;
-
-            _mockAuthenService
-                .Setup(service => service.VerifyOTP(It.IsAny<VerifyOtpDTO>()))
-                .ThrowsAsync(new ApplicationException(expectedMessage));
-
-            // Act
-            var result = await _controller.VerifyOTP(verifyOtpDto);
-
-            // Assert
-            Assert.False(result.Success);
-            Assert.Equal(200, result.Code);
-            Assert.Equal(expectedMessage, result.Message);
-
-            _mockAuthenService.Verify(service => service.VerifyOTP(verifyOtpDto), Times.Once);
         }
     }
 }
